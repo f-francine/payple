b337b527fca5c19f7171854a7a60388c30074ce1
Francine Guimarães <francinesguimaraes@outlook.com>
2021-02-28 02:10:13 -0300

    Create module Accounts.Deposit

--- lib/payple.ex
+++ lib/payple.ex
@@ -1,5 +1,8 @@
 defmodule Payple do
   alias Payple.Users.Create, as: UserCreate
+  alias Payple.Accounts.Deposit
 
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate deposit(params), to: Deposit, as: :call
 end
